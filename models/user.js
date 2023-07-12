e98f5d0b56539e5fe1d972f2e8097c9870bfd544
Julia Dikolenko <tsvetkova-julia-vl@yandex.ru>
2023-07-12 15:05:59 +0300

    настроить возвращение хеша пароля

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const UnauthorizedError = require('../errors/unauthorized-err');
 
 // схема пользователя
 const userSchema = new mongoose.Schema({
@@ -28,8 +30,34 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    // по умолчанию хеш пароля пользователя не будет возвращаться из БД
+    select: false,
   },
 });
+
+// в случае аутентификации разрешить возвращение хеша пароля пользователя из БД
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  // поиск пользователя с указанной почтой
+  // this — это модель User
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      // не нашелся — ошибка
+      if (!user) {
+        throw new UnauthorizedError('Неправильные почта или пароль');
+      }
+
+      // нашелся — сравниваем хеши
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          // не совпали - ошибка
+          if (!matched) {
+            throw new UnauthorizedError('Неправильные почта или пароль');
+          }
+          // совпали - возвращаем объект пользователя
+          return user;
+        });
+    });
+};
 
 // модель пользователя
 const User = mongoose.model('user', userSchema);
